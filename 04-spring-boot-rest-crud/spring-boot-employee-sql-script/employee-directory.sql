7b037080ee45ae66de1b04fcf6954da0126ff58f
Jude <judeufuomao@gmail.com>
2026-08-10 10:00:41 +0300

    added a new table

--- 04-spring-boot-rest-crud/spring-boot-employee-sql-script/employee-directory.sql
+++ 04-spring-boot-rest-crud/spring-boot-employee-sql-script/employee-directory.sql
@@ -1,18 +1,20 @@
 CREATE DATABASE  IF NOT EXISTS `employee_directory`;
 USE `employee_directory`;
 
+DROP TABLE IF EXISTS `authorities`;
+DROP TABLE IF EXISTS `users`;
+
 --
 -- Table structure for table `employee`
 --
 
-DROP TABLE IF EXISTS `employee`;
+DROP TABLE IF EXISTS `users`;
 
-CREATE TABLE `employee` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `users` (
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `enabled` tinyint NOT NULL,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 --
@@ -20,9 +22,24 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
+	('John','{noop}test123'),
+	('mary','{noop}test123'),
+	('susan','{noop}test123');
+    
+--
+-- Table structure for table `authorities`
+--
+
+CREATE TABLE `authorities` (
+	`username` varchar(50) NOT NULL,
+    `authority` varchar(50) NOT NULL,
+    UNIQUE KEY `authorities_idx_1` (`username`, `authority`),
+    CONSTRAINT `authorities_ibfk1_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+
+
 
